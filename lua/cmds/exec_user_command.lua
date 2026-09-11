592e9d4a9eb7a32844276a60065ef1d80e3bcded
Martin Braun <martin-braun@w9mail.com>
2026-09-11 23:20:29 +0200

    fix(cmds): Exec printing interactive shell warnings

--- lua/cmds/exec_user_command.lua
+++ lua/cmds/exec_user_command.lua
@@ -1,5 +1,9 @@
 vim.api.nvim_create_user_command("Exec", function(opts)
-    vim.cmd("r !" .. vim.env.SHELL .. " -ic " .. vim.fn.shellescape(opts.args) .. " 2>&1")
+    local cursor = vim.api.nvim_win_get_cursor(0)
+    local cmd = "exec 2>&1; " .. opts.args
+    vim.cmd("0r !" .. vim.env.SHELL .. " -ic " .. vim.fn.shellescape(cmd) .. " 2>/dev/null")
+    vim.cmd("$delete _")
+    vim.api.nvim_win_set_cursor(0, cursor)
 end, {
     nargs = "+",
 })
