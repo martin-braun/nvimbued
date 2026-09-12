d8dff312f9911a78c710e2eafec74771c978aaf4
Martin Braun <martin-braun@w9mail.com>
2026-09-13 01:39:53 +0200

    feat(ft): hide tabs for markdown

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,6 +1,7 @@
 local fmt = require("utils.fmt")
 vim.opt_local.colorcolumn = ""
 vim.opt_local.wrap = true
+fmt.buf_fillermod(false, false, false)
 if not fmt.buf_try_use({ "dprint", "prettier" }) then
     fmt.buf_disable_format()
 end
